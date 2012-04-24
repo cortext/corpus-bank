ba31bc4ee8808362ac302b8c88dee4044c569208
Duong Tam Kien <tk@deveha.com>
2012-04-24 12:05:47 +0200

    introducing count

--- corpus/__init__.py
+++ corpus/__init__.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template
+
+import glob
+
 from whoosh.index import open_dir
 from whoosh.fields import *
 from whoosh.qparser import QueryParser
@@ -6,6 +9,24 @@ from whoosh.query import *
 
 app = Flask(__name__)
 app.debug = True
+
+@app.route("/")
+def home():
+	r = glob.glob("repository/*")
+	
+	items = []
+	
+	for corpus_dir in r:
+		item = {}
+		item["name"] = corpus_dir.split("/",1)[1]
+		
+		ix = open_dir(corpus_dir)
+		
+		item["count"] = ix.doc_count()
+		
+		items.append(item)
+	
+	return render_template("home.html", items = items)
 
 @app.route("/corpus/<corpus_id>/schema")
 def schema(corpus_id):
